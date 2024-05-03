df2f68ca4a7bcedc14de0a6bb5817e22f3c36c3e
Matheus <matheus.silva246@fatec.sp.gov.br>
2024-05-02 22:19:01 -0300

    add link and removed link

--- Frontend/src/components/Header/index.jsx
+++ Frontend/src/components/Header/index.jsx
@@ -1,15 +1,15 @@
 import {RiShutDownLine } from 'react-icons/ri'
 import { useAuth } from '../../hooks/auth';
 import { api } from '../../services/api';
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg';
 
 
 import { Container, Profile, Logout } from "./style";
 
 export function Header(){
-    const { signOut,user } = useAuth();
+    const { signOut, user } = useAuth();
 
     const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
-
 
     return (
         <Container>
--- Frontend/src/pages/New/index.jsx
+++ Frontend/src/pages/New/index.jsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+
 import { Textarea } from '../../components/Textarea';
 import { NoteItem } from '../../components/NoteItem';
 import { Section } from '../../components/Section';
@@ -10,6 +12,18 @@ import { Input } from '../../components/Input';
 import { Container, Form } from './styles';
 
 export function New() {
+   const [links, setLinks] = useState([]);
+   const [newLink, setNewLink] = useState("");
+
+   function handleAddLink(){
+    setLinks(prevState => [...prevState, newLink]);
+    setNewLink("");
+   }
+   
+   function handleRemoveLink(deleted){
+        setLinks(prevState => prevState.filter(link => link !== deleted));
+   }
+
     return (
         <Container>
             <Header />
@@ -25,8 +39,22 @@ export function New() {
                     <Textarea placeholder="Observações" />
 
                     <Section title="Links úteis">
-                        <NoteItem value="https://rocketseat.com.br" />
-                        <NoteItem isNew placeholder="Novo link" />
+                        {
+                            links.map((link, index) => (
+                                <NoteItem 
+                                    key={String(index)}
+                                    value={link}
+                                    onClick={() => handleRemoveLink(link)}
+                                />
+                            ))
+                        }
+                        <NoteItem 
+                            isNew 
+                            placeholder="Novo link" 
+                            value={newLink}
+                            onChange={e => setNewLink(e.target.value)}
+                            onClick={handleAddLink}
+                        />
                     </Section>
 
                     <Section title="Marcadores">
