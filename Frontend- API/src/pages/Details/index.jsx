b70dd56926de77ecac3dea51438ea9f831820510
Matheus <matheus.silva246@fatec.sp.gov.br>
2024-08-10 17:20:05 -0300

    att deatails

--- Frontend- API/src/pages/Details/index.jsx
+++ Frontend- API/src/pages/Details/index.jsx
@@ -1,4 +1,8 @@
+import { useState, useEffect } from 'react';
 import { Container, Links, Content } from './style';
+import { useParams, useNavigate } from 'react-router-dom';
+
+import { api } from '../../services/api';
 
 import { Tag } from '../../components/Tag';
 import { Button } from '../../components/Button';
@@ -10,41 +14,79 @@ import { ButtonText } from '../../components/ButtonText';
 
 
 export function Details(){
+  const [data, setData] = useState(null);
+
+  const params = useParams();
+  const navigate = useNavigate();
+
+  function handleBack(){
+    navigate("/");
+  }
+
+
+  useEffect(() => {
+    async function fetchNote(){
+      const response = await api.get(`/notes/${params.id}`);
+      setData(response.data);
+    }
+
+    fetchNote();
+  }, []);
 
   return(
     <Container>
       <Header />
-
-      <main>
+      {
+        data && 
+        <main>
         <Content>
           <ButtonText title="Excluir nota" />
 
           <h1>
-            Introdução ao React
+            {data.title}
           </h1>
 
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing 
-            elit. Perspiciatis non nostrum nobis amet velit, 
-            corporis sequi eligendi, earum debitis cumque ducimus, 
-            neque vitae recusandae. Facere labore repudiandae ad 
-            illum magni?
+           {data.description}
           </p>
-          <Section title="Links úteis">
-          <Links>
-            <li><a href="#">https://www.rocketseat.com.br/</a></li>
-            <li><a href="#">https://www.rocketseat.com.br/</a></li>
-          </Links>
-          </Section>
+          {
+            data.links &&
+            <Section title="Links úteis">
+              <Links>
+                {
+                  data.links.map(link => (
+                    <li key={String(link.id)}>
+                      <a href={link.url} target="_blank">
+                        {link.url}
+                      </a>
+                    </li>
+                  ))
+                }
+              </Links>
+            </Section>
+          }
 
-          <Section title="Marcadores">
-            <Tag title="express" />
-            <Tag title="node.js" />
-          </Section>
-
-          <Button title="Voltar" />
+          {
+            data.tags &&
+            <Section title="Marcadores">
+              {
+                data.tags.map(tag => (
+                  <Tag 
+                    key={String(tag.id)}
+                    title={tag.name} 
+                  />
+                ))
+              }
+            </Section>
+          }
+          
+          <Button 
+            title="Voltar" 
+            onClick={handleBack} 
+          />
         </Content>
-      </main>
+        </main>
+      }
     </Container>
   );
 }
--- Frontend- API/src/pages/Home/index.jsx
+++ Frontend- API/src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import { useState,useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
 
 import { api } from '../../services/api';
@@ -18,6 +19,8 @@ export function Home(){
     const [tagsSelected, setTagsSelected] = useState([]);
     const [notes, setNotes] = useState([]);
 
+    const navigate = useNavigate();
+
     function handleTagSelected(tagName){
         if(tagName === "all"){
             return setTagsSelected([]);
@@ -33,6 +36,10 @@ export function Home(){
             setTagsSelected(prevState => [...prevState, tagName]);  
 
         }
+    }
+
+    function handleDetails(id){
+        navigate(`/details/${id}`);
     }
 
     useEffect(() => {
@@ -87,7 +94,7 @@ export function Home(){
             <Search>
                 <Input  
                     placeholder="Pesquisar pelo titulo"  
-                    onChange={() => setSearch(e.target.valeu)}
+                    onChange={(e) => setSearch(e.target.value)}
                 />
             </Search>
         
@@ -97,7 +104,8 @@ export function Home(){
                         notes.map(note => (  
                             <Note 
                                 key={String(note.id)}
-                                data={note} 
+                                data={note}
+                                onClick={() => handleDetails(note.id)}
                             />
                         ))
                     }
