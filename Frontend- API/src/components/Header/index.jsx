be7d847738334567bccebbffd70cf52bb7a3e8a8
Matheus <matheus.silva246@fatec.sp.gov.br>
2024-08-10 18:47:19 -0300

    att routes

--- Frontend- API/src/components/Header/index.jsx
+++ Frontend- API/src/components/Header/index.jsx
@@ -1,13 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+
 import {RiShutDownLine } from 'react-icons/ri'
 import { useAuth } from '../../hooks/auth';
-import { api } from '../../services/api';
-import avatarPlaceholder from '../../assets/avatar_placeholder.svg';
 
+import { api } from '../../services/api';
+
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg';
 
 import { Container, Profile, Logout } from "./style";
 
 export function Header(){
     const { signOut, user } = useAuth();
+    const navigation = useNavigate();
+
+    function handleSignOut(){
+        navigation("/");
+        signOut();
+    }
 
     const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
 
@@ -24,7 +33,7 @@ export function Header(){
                 </div>
             </Profile>
 
-            <Logout onClick={signOut}>
+            <Logout onClick={handleSignOut}>
                 <RiShutDownLine />
             </Logout>
 
--- Frontend- API/src/routes/auth.routes.jsx
+++ Frontend- API/src/routes/auth.routes.jsx
@@ -1,14 +1,18 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import { SignIn } from '../pages/SignIn';
 import { SignUP } from '../pages/SignUP';
 
 
 export function AuthRoutes() {
+    const user = localStorage.getItem("@rocketnotes:user");
+
     return(
         <Routes>
             <Route path="/" element={<SignIn />} />
             <Route path="/register" element={<SignUP />} />
+
+            { !user && <Route path="*" element={<Navigate to="/" />} />}
         </Routes>
     )
 }
