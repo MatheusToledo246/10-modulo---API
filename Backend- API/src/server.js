8be48cc676352771dd5af2ceadf3e9ecb707f770
Matheus <matheus.silva246@fatec.sp.gov.br>
2024-08-14 20:53:09 -0300

    add pm2

--- Backend- API/src/server.js
+++ Backend- API/src/server.js
@@ -1,13 +1,16 @@
 require("express-async-errors");
-const migrationsRun = require('./database/sqlite/migrations')
-const AppError = require("./utils/AppError");
+require("dotenv/config");
+
 const uploadConfig = require("./configs/upload");
 
-const cors = require("cors");
+const migrationsRun = require('./database/sqlite/migrations')
+const AppError = require("./utils/AppError");
+
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes");
 
-migrationsRun()
+migrationsRun();
 
 const app = express();
 app.use(cors());
@@ -33,6 +36,6 @@ app.use((error, request, response, next ) => {
     })
 });
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
 
